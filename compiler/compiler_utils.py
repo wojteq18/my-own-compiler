a963aacf858e47445a29de5c64b00dda5c23f85f
wojteq18 <279730@student.pwr.edu.pl>
2026-01-18 15:24:21 +0100

    ADDED: procedures

--- compiler/compiler_utils.py
+++ compiler/compiler_utils.py
@@ -2,71 +2,60 @@ from register_manager import reg_manager
 
 free_memory_address = 8
 symbols_table = {}
+procedures_table = {}
 
-def get_addr(variable_name): #Przyporządkowuje unikalny adres w pamięci dla zmiennej
+def get_addr(variable_name):
     global free_memory_address
     global symbols_table
     if variable_name not in symbols_table:
-        symbols_table[variable_name] = free_memory_address
+        symbols_table[variable_name] = {'addr': free_memory_address}
         free_memory_address += 1
-    return symbols_table[variable_name]
+    return symbols_table[variable_name]['addr']
 
 def generate_number(number_value, buffer):
     buffer.add_instr("RST a")
     if number_value == 0:
         return
-    binary_repr = bin(number_value)[2:]  # Pomijamy prefiks '0b'
-    buffer.add_instr("INC a")  # Inicjalizuj a na 1
-    for bit in binary_repr[1:]:  # Pomijamy pierwszy bit, bo już mamy 1
-        buffer.add_instr("SHL a")  # Przesuń w lewo (mnożenie przez 2)
+    binary_repr = bin(number_value)[2:]
+    buffer.add_instr("INC a")
+    for bit in binary_repr[1:]:
+        buffer.add_instr("SHL a")
         if bit == '1':
-            buffer.add_instr("INC a")  # Dodaj 1 jeśli bit to 1
+            buffer.add_instr("INC a")
       
-  
 def gen_multiply(buffer, reg_m1):
     reg_m2 = reg_manager.get_register()   
     reg_res = reg_manager.get_register()  
     reg_temp = reg_manager.get_register() 
-
     l_start = buffer.get_new_label()
     l_add = buffer.get_new_label()
     l_end = buffer.get_new_label()
-
     buffer.add_instr(f"SWP {reg_m2}")     
     buffer.add_instr(f"RST {reg_res}")   
-
     buffer.set_label(l_start)
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_m2}")
     buffer.add_instr(f"JZERO LABEL_{l_end}") 
-
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_m2}")
     buffer.add_instr("SHR a")
     buffer.add_instr("SHL a")
-    
     buffer.add_instr(f"RST {reg_temp}") 
     buffer.add_instr(f"SWP {reg_temp}") 
-    
     buffer.add_instr(f"ADD {reg_m2}")
     buffer.add_instr(f"SUB {reg_temp}") 
-    
     buffer.add_instr(f"JZERO LABEL_{l_add}") 
-
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_res}")
     buffer.add_instr(f"ADD {reg_m1}")
     buffer.add_instr(f"SWP {reg_res}")
-
     buffer.set_label(l_add)
     buffer.add_instr(f"SHL {reg_m1}")     
     buffer.add_instr(f"SHR {reg_m2}")    
     buffer.add_instr(f"JUMP LABEL_{l_start}") 
-
     buffer.set_label(l_end)
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_res}")    
-
     reg_manager.release_register()
     reg_manager.release_register()
     reg_manager.release_register()
@@ -77,87 +66,70 @@ def get_divide(buffer, reg_m1, operator):
     reg_b_shift = reg_manager.get_register() 
     reg_mult = reg_manager.get_register()    
     reg_temp = reg_manager.get_register()    
-
     l_div_zero = buffer.get_new_label()
     l_align_start = buffer.get_new_label()
     l_align_end = buffer.get_new_label()
     l_loop_start = buffer.get_new_label()
     l_skip_sub = buffer.get_new_label()
     l_end = buffer.get_new_label()
-
     buffer.add_instr(f"SWP {reg_m2}") 
     buffer.add_instr(f"RST {reg_q}")  
-
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_m2}")
     buffer.add_instr(f"JZERO LABEL_{l_div_zero}")
-
     buffer.add_instr(f"RST a")
     buffer.add_instr(f"RST {reg_b_shift}")
     buffer.add_instr(f"ADD {reg_m2}")
     buffer.add_instr(f"SWP {reg_b_shift}")
-    
     buffer.add_instr(f"RST a") 
     buffer.add_instr(f"RST {reg_mult}")
     buffer.add_instr("INC a")  
     buffer.add_instr(f"SWP {reg_mult}")
-
     buffer.set_label(l_align_start)
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_b_shift}")
     buffer.add_instr("SHL a")
     buffer.add_instr(f"RST {reg_temp}")
     buffer.add_instr(f"SWP {reg_temp}") 
-        
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_temp}")
     buffer.add_instr(f"SUB {reg_m1}") 
     buffer.add_instr(f"JPOS LABEL_{l_align_end}") 
-        
     buffer.add_instr(f"SHL {reg_b_shift}")
     buffer.add_instr(f"SHL {reg_mult}")
     buffer.add_instr(f"JUMP LABEL_{l_align_start}")
     buffer.set_label(l_align_end)
-
     buffer.set_label(l_loop_start)
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_b_shift}")
     buffer.add_instr(f"SUB {reg_m1}")
     buffer.add_instr(f"JPOS LABEL_{l_skip_sub}") 
-
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_m1}")
     buffer.add_instr(f"SUB {reg_b_shift}")
     buffer.add_instr(f"SWP {reg_m1}")
-        
     buffer.add_instr("RST a") 
     buffer.add_instr(f"ADD {reg_q}")
     buffer.add_instr(f"ADD {reg_mult}")
     buffer.add_instr(f"SWP {reg_q}")
-
     buffer.set_label(l_skip_sub)
     buffer.add_instr(f"SHR {reg_b_shift}")
     buffer.add_instr(f"SHR {reg_mult}")
-        
     buffer.add_instr("RST a")
     buffer.add_instr(f"ADD {reg_mult}")
     buffer.add_instr(f"JPOS LABEL_{l_loop_start}")
     buffer.add_instr(f"JUMP LABEL_{l_end}")
-
     buffer.set_label(l_div_zero)
     buffer.add_instr(f"RST {reg_q}")
     buffer.add_instr(f"RST {reg_m1}")
-
     buffer.set_label(l_end)
     buffer.add_instr("RST a")
     if operator == "DIVIDE":
         buffer.add_instr(f"ADD {reg_q}")
     else:
         buffer.add_instr(f"ADD {reg_m1}")
-
     for _ in range(5):
         reg_manager.release_register()
-
 
 def get_array_addr(name, start, end):
     global free_memory_address
@@ -165,20 +137,46 @@ def get_array_addr(name, start, end):
     if name in symbols_table:
         import sys
         sys.exit(f"Error: Variable/Array '{name}' already declared")
-    
     start = int(start)
     end = int(end)
     if start > end:
         import sys
         sys.exit(f"Error: Invalid array range [{start}:{end}]") 
-
     size = end - start + 1
     base_addr = free_memory_address
-    symbols_table[name] = {
-        'type': 'array',
-        'addr': base_addr,
-        'start': start,
-        'end': end
-    }
+    symbols_table[name] = {'type': 'array', 'addr': base_addr, 'start': start, 'end': end}
     free_memory_address += size
     return base_addr
+
+def get_proc_param_addr(proc_name, param_index):
+    global free_memory_address
+    global symbols_table
+    key = f"__proc_{proc_name}_param_{param_index}"
+    if key not in symbols_table:
+        symbols_table[key] = {'addr': free_memory_address, 'type': 'pointer'}
+        free_memory_address += 1
+    return symbols_table[key]['addr']
+
+class CodeBuffer: 
+    def __init__(self):
+        self.instructions = []
+        self.labels = {}
+        self.label_counter = 0
+    def get_new_label(self):
+        self.label_counter += 1
+        return f"L{self.label_counter}"
+    def add_instr(self, instr):
+        self.instructions.append(instr)
+    def set_label(self, label_name):
+        self.labels[label_name] = len(self.instructions)
+    def finalize(self):
+        final_output = []
+        sorted_labels = sorted(self.labels.items(), key=lambda x: len(x[0]), reverse=True)
+        for instr in self.instructions:
+            processed_instr = instr
+            for label, line_num in sorted_labels:
+                placeholder = f"LABEL_{label}"
+                if placeholder in processed_instr:
+                    processed_instr = processed_instr.replace(placeholder, str(line_num))    
+            final_output.append(processed_instr)
+        return "\n".join(final_output)
--- compiler/parser.py
+++ compiler/parser.py
@@ -1,24 +1,104 @@
 import ply.yacc as yacc
 from lexer import tokens
-from compiler_utils import get_addr, symbols_table, free_memory_address, generate_number, get_array_addr
+from compiler_utils import get_addr, symbols_table, free_memory_address, generate_number, get_array_addr, procedures_table
 import sys
 from register_manager import reg_manager
-from abstract_syntax_tree import AssignmentNode, NumberNode, BinaryOperationNode, ReadNode, VariableNode, WhileNode, WriteNode, ArrayElementNode, IfNode, ConditionNode, ForNodeTo, ForNodeDownTo, RepeatNode
+from abstract_syntax_tree import AssignmentNode, CallNode, NumberNode, BinaryOperationNode, ProcedureNode, ReadNode, VariableNode, WhileNode, WriteNode, ArrayElementNode, IfNode, ConditionNode, ForNodeTo, ForNodeDownTo, RepeatNode
 from code_buffer import CodeBuffer
+
+current_scope = "main"
 
 precedence = (
     ('left', 'ADD', 'MINUS'),
     ('left', 'MULTIPLY', 'DIVIDE', 'MODULO'),
 )
 
-def p_program(p):
-    'program : PROGRAM IS declarations IN commands END'
+def p_program_all(p):
+    'program : procedures main'
     buffer = CodeBuffer()
-    for cmd in p[5]:
+    buffer.add_instr("JUMP LABEL_MAIN")
+    for proc_node in p[1]:
+        proc_node.generate(buffer)
+    buffer.set_label("MAIN")
+    for cmd in p[2]:
         cmd.generate(buffer)
-
     buffer.add_instr("HALT")
-    p[0] = buffer.finalize()    
+    p[0] = buffer.finalize()
+
+def p_procedures_multiple(p):
+    '''procedures : procedures PROCEDURE proc_head IS declarations IN commands END
+                  | procedures PROCEDURE proc_head IS IN commands END'''
+    name, params = p[3]
+    commands = p[7] if len(p) == 9 else p[6]
+    node = ProcedureNode(name, params, commands)
+    global current_scope
+    current_scope = "main"
+    p[0] = p[1] + [node]
+
+def p_procedures_empty(p):
+    'procedures : '
+    p[0] = []    
+
+def p_proc_head(p):
+    'proc_head : ID LPAREN args_decl RPAREN'
+    name, params = p[1], p[3]
+    if name in procedures_table:
+        sys.exit(f"Error: Procedure {name} already defined") 
+    global current_scope
+    current_scope = name
+    param_addrs = []
+    for i, (p_name, p_type) in enumerate(params):
+        full_name = f"{current_scope}_{p_name}"
+        addr = get_addr(full_name)
+        symbols_table[full_name]['is_param'] = True
+        symbols_table[full_name]['param_type'] = p_type
+        param_addrs.append(addr)
+    procedures_table[name] = {'args_count': len(params), 'params': params, 'param_addrs': param_addrs}
+    p[0] = (name, params)  
+
+def p_args_decl_multi(p):
+    'args_decl : args_decl COMMA arg_type ID'
+    p[0] = p[1] + [(p[4], p[3])] 
+
+def p_args_decl_single(p):
+    'args_decl : arg_type ID'
+    p[0] = [(p[2], p[1])]     
+
+def p_arg_type(p):
+    '''arg_type : T I O
+                | T I
+                | T O
+                | T
+                | I
+                | O
+                | empty'''
+    p[0] = "".join([str(x) for x in p[1:] if x is not None]) 
+
+def p_main_node(p):
+    '''main : PROGRAM IS declarations IN commands END
+            | PROGRAM IS IN commands END'''
+    p[0] = p[5] if len(p) == 7 else p[4]   
+
+def p_proc_call(p):
+    'command : ID LPAREN args RPAREN SEMICOLON'
+    name, args = p[1], p[3]
+    if name not in procedures_table:
+        sys.exit(f"Błąd: Nieznana procedura {name}") 
+    if len(args) != procedures_table[name]['args_count']:
+        sys.exit(f"Błąd: Zła liczba argumentów w {name}")
+    p[0] = CallNode(name, args)    
+
+def p_args_multi(p):
+    'args : args COMMA ID'
+    p[0] = p[1] + [VariableNode(f"{current_scope}_{p[3]}")]    
+
+def p_args_single(p):
+    'args : ID'
+    p[0] = [VariableNode(f"{current_scope}_{p[1]}")]    
+
+def p_empty(p):
+    'empty :'
+    p[0] = None             
 
 def p_commands_multiple(p):
     'commands : commands command'
@@ -44,28 +124,21 @@ def p_value_id(p):
     'value : identifier'
     p[0] = p[1]       
 
-
 def p_variable_declaration_single(p):
     'declarations : ID'
-    var_name = p[1]
-    addr = get_addr(var_name)
-    print(f"Zmienna {p[1]} zarejestrowana pod adresem {addr}") 
+    get_addr(f"{current_scope}_{p[1]}")
 
 def p_assign_command(p):
     'command : identifier ASSIGN expression SEMICOLON'
     p[0] = AssignmentNode(p[1], p[3])
 
-
 def p_expression_value(p):
     'expression : value'
     p[0] = p[1]
 
-
 def p_variable_declaration_multiple(p):
     'declarations : declarations COMMA ID'
-    var_name = p[3]
-    addr = get_addr(var_name)
-    print(f"Zmienna {p[3]} zarejestrowana pod adresem {addr}") 
+    get_addr(f"{current_scope}_{p[3]}")
 
 def p_expression_addition(p):
     'expression : expression ADD expression'
@@ -101,14 +174,14 @@ def p_command_repeatuntil(p):
 
 def p_for_iterator(p):
     'for_iterator : FOR ID'
-    if p[2] not in symbols_table:
-        get_addr(p[2])
-    p[0] = p[2]        
+    full_name = f"{current_scope}_{p[2]}"
+    if full_name not in symbols_table:
+        get_addr(full_name)
+    p[0] = full_name        
 
 def p_command_fordownto(p):
     'command : for_iterator FROM value DOWNTO value DO commands ENDFOR' 
     p[0] = ForNodeDownTo(p[1], p[3], p[5], p[7])    
-
 
 def p_condition_less(p):
     'condition : expression LESS expression'
@@ -150,27 +223,38 @@ def p_variable_declaration_array(p):
     '''declarations : ID LBRACKET NUMBER COLON NUMBER RBRACKET
                     | declarations COMMA ID LBRACKET NUMBER COLON NUMBER RBRACKET'''
     if len(p) == 7:
-        get_array_addr(p[1], p[3], p[5])
+        get_array_addr(f"{current_scope}_{p[1]}", p[3], p[5]) 
     else: 
-        get_array_addr(p[3], p[5], p[7])    
+        get_array_addr(f"{current_scope}_{p[3]}", p[5], p[7]) 
 
 def p_identifier_simple(p):
     'identifier : ID'
-    if p[1] not in symbols_table:
-        sys.exit(f"Error: Variable '{p[1]}' not declared")
-    p[0] = VariableNode(p[1])
+    full_name = f"{current_scope}_{p[1]}"
+    if full_name not in symbols_table:
+        sys.exit(f"Error: Variable '{p[1]}' not declared in scope {current_scope}") 
+    p[0] = VariableNode(full_name)
 
 def p_identifier_array_var(p):
     'identifier : ID LBRACKET ID RBRACKET'
-    if p[3] not in symbols_table:
-        sys.exit(f"Error: Variable '{p[3]}' not declared")
-    p[0] = ArrayElementNode(p[1], VariableNode(p[3]))
+    full_name_arr = f"{current_scope}_{p[1]}"
+    full_name_idx = f"{current_scope}_{p[3]}"
+    if full_name_arr not in symbols_table:
+        sys.exit(f"Error: Array '{p[1]}' not declared") 
+    if full_name_idx not in symbols_table:
+        sys.exit(f"Error: Index variable '{p[3]}' not declared") 
+    p[0] = ArrayElementNode(full_name_arr, VariableNode(full_name_idx))
 
 def p_identifier_array_num(p):
     'identifier : ID LBRACKET NUMBER RBRACKET'
-    p[0] = ArrayElementNode(p[1], NumberNode(p[3]))        
+    full_name_arr = f"{current_scope}_{p[1]}"
+    if full_name_arr not in symbols_table:
+        sys.exit(f"Error: Array '{p[1]}' not declared") 
+    p[0] = ArrayElementNode(full_name_arr, NumberNode(p[3]))        
 
 def p_error(p):
-    print(f"Error in syntax in line {p.lineno}")      
-             
+    if p:
+        print(f"Error in syntax in line {p.lineno} at token {p.value}")      
+    else:
+        print("Error in syntax at EOF")
+
 parser = yacc.yacc()
